64d282cd40093061a1598ab3590cdb3b50a5709c
Giseli Almeida <giselinnes@hotmail.com>
2021-10-03 20:21:57 -0300

    Create 003_Soma_entre_dois_numeros.py

---
+++ 003_Soma_entre_dois_numeros.py
@@ -0,0 +1,6 @@
+print('===Soma entre dois números===')
+n1 = float(input('Didite o primeiro número:'))
+n2 = int(input('Digite o segundo número:'))
+soma = n1+n2
+print('A soma dos valores', n1, 'e', n2, 'é:', soma)
+
